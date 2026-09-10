061438b3dc3e4eed8e468ccb20ae0aa8246e8934
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2026-09-10 09:04:37 +0200

    Add home

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+// src/Controller/HomeController.php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function home(): Response
+    {
+        return $this->render('Default/home.html.twig', []);
+    }
+}
